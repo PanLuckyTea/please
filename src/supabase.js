2f96d638375b1eb491be2bb236fdbb4ddf1d7f13
PanLuckyTea <dziedzicki@student.agh.edu.pl>
2026-06-13 16:05:42 +0200

    15

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://vymnkwxmmddrwpuzeldk.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5bW5rd3htbWRkcndwdXplbGRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzMzcyMTMsImV4cCI6MjA5NjkxMzIxM30.kxxfVIZMYzhR3_XKZrVcgJWg5zI3EZpOAxUMiuaIB78';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://vymnkwxmmddrwpuzeldk.supabase.co";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "TWÓJ_KLUCZ_ANON_KEY";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("Brakuje kluczy Supabase! Sprawdź plik .env lub konfigurację.");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
